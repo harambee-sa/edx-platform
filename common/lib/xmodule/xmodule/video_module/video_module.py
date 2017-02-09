65b2c8ba1f84050129024e5661836361b00c88b6
Renevatium <rene@developmental.co.za>
2017-02-09 11:49:31 +0000

    fix for video duration assignment bug

--- common/lib/xmodule/xmodule/video_module/video_module.py
+++ common/lib/xmodule/xmodule/video_module/video_module.py
@@ -554,6 +554,7 @@ class VideoDescriptor(VideoFields, VideoTranscriptsMixin, VideoStudioViewHandler
                 video_url = "https://www.youtube.com/watch?v=" + str(self.youtube_id_1_0)
 
             if video_url:
+                
                 if not self.edx_video_id:
                     now = datetime.datetime.now()
                     hash_object = hashlib.sha256(str(now))
@@ -576,9 +577,10 @@ class VideoDescriptor(VideoFields, VideoTranscriptsMixin, VideoStudioViewHandler
 
                 # TODO: Change this try catch
                 try:
-                    edxval_api.get_video_info(hex_dig)
+                    edxval_api.get_video_info(self.edx_video_id)
                 except:
                     edxval_api.create_video(payload)
+                    
                 # with this
                 # edxval_api.create_video(payload)
                 # when edxval app is updated
