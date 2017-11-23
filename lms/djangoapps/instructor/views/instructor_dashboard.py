9f93343f6457e37e96ac7f188bfe61203ade28f5
Nico van Niekerk <nico@proversity.org>
2017-11-23 20:04:53 +0200

    Proversity/development (#576)
    FIX: username not respected
    FIX: speed inprovements on grades api
    FIX: error handeling around course not exist
    FIX: error handling around username exists but does not match email combination
    FIX: returning of historical grades

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -487,7 +487,11 @@ def _section_course_info(course, access):
         'course_org': course.display_org_with_default,
         'course_number': course.display_number_with_default,
         'course_name': course.display_name,
+<<<<<<< HEAD
         'course_display_name': course.display_name,
+=======
+        'course_display_name': course.display_name_with_default,
+>>>>>>> Proversity/development (#576)
         'has_started': course.has_started(),
         'has_ended': course.has_ended(),
         'start_date': course.start,
