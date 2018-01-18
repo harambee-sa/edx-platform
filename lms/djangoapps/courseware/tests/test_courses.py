c6910c4ab34ad9f144b9f5247cdb36893c0e1069
lidijarad <radovanovic.lidija22@gmail.com>
2018-01-18 13:04:21 +0200

    add tests

--- lms/djangoapps/courseware/tests/test_courses.py
+++ lms/djangoapps/courseware/tests/test_courses.py
@@ -166,6 +166,31 @@ class CoursesTest(ModuleStoreTestCase):
                     get_courses(user, filter_=filter_)
                 },
                 expected_courses,
+                "testing get_courses with language filter_={}".format(filter_),
+            )
+
+
+    def test_get_courses_with_languague_filter(self):
+        """
+        Verify that filtering performs as expected.
+        """
+        user = UserFactory.create()
+        non_mobile_course = CourseFactory.create(emit_signals=True)
+        mobile_course = CourseFactory.create(mobile_available=True, emit_signals=True)
+
+        test_cases = (
+            (None, {non_mobile_course.id, mobile_course.id}),
+            (dict(mobile_available=True), {mobile_course.id}),
+            (dict(mobile_available=False), {non_mobile_course.id}),
+        )
+        for filter_, expected_courses in test_cases:
+            self.assertEqual(
+                {
+                    course.id
+                    for course in
+                    get_courses(user, filter_=filter_)
+                },
+                expected_courses,
                 "testing get_courses with filter_={}".format(filter_),
             )
 
